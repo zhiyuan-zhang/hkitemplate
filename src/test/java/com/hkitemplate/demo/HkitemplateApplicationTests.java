ca3f6804ba03714eb2849e9bc76d69c1b7525867
apple <apple@appletekiMacBook-Air.local>
2018-09-07 22:12:50 +0800

    "add test"

--- src/test/java/com/hkitemplate/demo/HkitemplateApplicationTests.java
+++ src/test/java/com/hkitemplate/demo/HkitemplateApplicationTests.java
@@ -1,16 +1,32 @@
 package com.hkitemplate.demo;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.alibaba.fastjson.JSON;
+import com.hkitemplate.demo.beans.DemoInfo;
+import com.hkitemplate.demo.dao.UserInfoMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HkitemplateApplicationTests {
 
+	@Autowired
+	UserInfoMapper userinfomapper;
+	
 	@Test
 	public void contextLoads() {
+		System.out.println("123123");
+		List<DemoInfo> findById = userinfomapper.findById(1);
+		System.out.println("\n\n\n\n\n");
+		System.out.println("输出结果：" + JSON.toJSONString(findById.get(0)));
+		System.out.println("输出结果：" + JSON.toJSONString(findById));
+		System.out.println("\n\n\n\n\n");
 	}
 
 }
